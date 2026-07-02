b82e4974f9918f3c8c8696d1dd710ece7d613967
lidongxing <lidx@bnu.edu.cn>
2026-07-02 21:01:52 +0800

    Exports get_dataloader for experiments and benchmarks.

---
+++ nlh_ssm/data/__init__.py
@@ -0,0 +1,6 @@
+"""Dataset loading utilities."""
+
+from nlh_ssm.data.loader import HIERARCHY_PRESETS, HierarchyMeta, get_dataloader, max_series_length_long
+
+__all__ = ["HierarchyMeta", "HIERARCHY_PRESETS", "get_dataloader", "max_series_length_long"]
+
